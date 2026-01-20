a06bce82433a9d08f4d0c0d49dff342a78ea8cf4
Justin Saju <justinsaju21@gmail.com>
2026-01-20 21:36:37 +0530

    Add Google Adsense script placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/ui/Navbar";
 import { Footer } from "@/components/ui/Footer";
+import GoogleAdsense from "@/components/GoogleAdsense";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
         <Navbar />
         <main>{children}</main>
         <Footer />
+        <GoogleAdsense pId="ca-pub-XXXXXXXXXXXXXXXX" />
       </body>
     </html>
   );

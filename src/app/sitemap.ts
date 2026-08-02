7618502f201c46114f226bbe3afb9b3e31a3131d
Justin Saju <justinsaju21@gmail.com>
2026-08-02 13:51:09 +0530

    Fix: Add auto-revalidation to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { getPosts } from '@/lib/sheets'
 
+export const revalidate = 3600 // Revalidate every hour
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://blog.justinsaju.me'
 
